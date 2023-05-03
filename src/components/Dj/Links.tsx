667012a4aba99d8089c593dc7cf6229dd1e63724
jafidiesel <jafibravin@outlook.com>
2023-05-03 12:20:31 -0300

    Added links ids

--- src/components/Dj/Links.tsx
+++ src/components/Dj/Links.tsx
@@ -12,19 +12,14 @@ const Links = () => (
         target="_blank"
         rel="noopener noreferrer"
         className={css(styles.itemLink)}
+        id="link-card-set-002"
       >
         <div className={css(styles.itemLinkInner)}>
           <div>
-            <FontAwesomeIcon
-              icon={faSoundcloud}
-              className={css(styles.darkIcon)}
-            />
+            <FontAwesomeIcon icon={faSoundcloud} className={css(styles.darkIcon)} />
             Set 002 - Luppulo
           </div>
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className={css(styles.darkIcon)}
-          />
+          <FontAwesomeIcon icon={faExternalLinkAlt} className={css(styles.darkIcon)} />
         </div>
       </a>
     </div>
@@ -34,19 +29,14 @@ const Links = () => (
         target="_blank"
         rel="noopener noreferrer"
         className={css(styles.itemLink)}
+        id="link-card-git-podcast-session-62"
       >
         <div className={css(styles.itemLinkInner)}>
           <div>
-            <FontAwesomeIcon
-              icon={faSoundcloud}
-              className={css(styles.darkIcon)}
-            />
+            <FontAwesomeIcon icon={faSoundcloud} className={css(styles.darkIcon)} />
             GIT Podcast Session 62 # Jafi Bravin Into The Mix 🇩🇪
           </div>
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className={css(styles.darkIcon)}
-          />
+          <FontAwesomeIcon icon={faExternalLinkAlt} className={css(styles.darkIcon)} />
         </div>
       </a>
     </div>
@@ -56,19 +46,14 @@ const Links = () => (
         target="_blank"
         rel="noopener noreferrer"
         className={css(styles.itemLink)}
+        id="link-card-after-movie-beatroom-showcase-2022"
       >
         <div className={css(styles.itemLinkInner)}>
           <div>
-            <FontAwesomeIcon
-              icon={faYoutube}
-              className={css(styles.darkIcon)}
-            />
+            <FontAwesomeIcon icon={faYoutube} className={css(styles.darkIcon)} />
             After Movie - Beatroom showcase 2022
           </div>
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className={css(styles.darkIcon)}
-          />
+          <FontAwesomeIcon icon={faExternalLinkAlt} className={css(styles.darkIcon)} />
         </div>
       </a>
     </div>
@@ -78,19 +63,14 @@ const Links = () => (
         target="_blank"
         rel="noopener noreferrer"
         className={css(styles.itemLink)}
+        id="link-card-set-001"
       >
         <div className={css(styles.itemLinkInner)}>
           <div>
-            <FontAwesomeIcon
-              icon={faSoundcloud}
-              className={css(styles.darkIcon)}
-            />
+            <FontAwesomeIcon icon={faSoundcloud} className={css(styles.darkIcon)} />
             Set 001 - Progressive
           </div>
-          <FontAwesomeIcon
-            icon={faExternalLinkAlt}
-            className={css(styles.darkIcon)}
-          />
+          <FontAwesomeIcon icon={faExternalLinkAlt} className={css(styles.darkIcon)} />
         </div>
       </a>
     </div>
